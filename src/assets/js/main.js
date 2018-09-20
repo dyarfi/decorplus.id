44336d02712e73edf1229d6cccedac87a5fffcd7
dyarfi <defrian.yarfi@gmail.com>
2018-09-20 11:06:01 +0700

    Assets and Index layouts

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,4 +8,21 @@ $(document).ready(function() {
     $('.button-close a[class^=btn]').on('click',function () {
         fixed_menu.toggleClass('invisible');
     });
+    // Slick Carousel on reviews
+    $('.slick-carousel').slick({        
+        centerPadding: 0,
+        focusOnSelect: false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: true,
+        speed: 1800,
+        centerMode: true,
+        cssEase: 'ease-in',
+        easing: 'ease-in-out',
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        waitForAnimate: false,
+        nextArrow: '<a href="javascript:;" class="carousel-control-next h3" data-slide="prev"><i class="oi oi-chevron-right"></i></a>',
+        prevArrow: '<a href="javascript:;" class="carousel-control-prev h3" data-slide="next"><i class="oi oi-chevron-left"></i></a>'
+    });
 });
